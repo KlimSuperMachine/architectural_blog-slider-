9940b76f6dd731bbc8a50c656302c3384764ebde
Клим <klimvorobev@mail.ru>
2022-06-20 22:43:29 +0300

    Доделал главную страницу, подключил слайдер

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      dots: true,
+      speed: 1500,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnDotsHover: true,
+      waitForAnimate: false,
+   });
+})
+
+$(document).ready(function () {
+   $('.slider1').slick({
+      dots: true,
+      speed: 1500,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnDotsHover: true,
+      waitForAnimate: false,
+      fade: true,
+   });
+})
